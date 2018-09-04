e7977c4bccca3bd4f70e40f396369e756b3e3052
fabioiuri <fabioiuri@live.com>
2018-09-04 21:22:55 +0100

    Add comparison between sklearn perceptron model vs perecptron built by me

--- PLA/First_PLA/main.py
+++ PLA/First_PLA/main.py
@@ -1,10 +1,13 @@
 # Perceptron Learning Algorithm 
-# Sample Usage and convergence test
+# Sample Usage and convergence test of perceptron on classification problem
+# Compare results of built perceptron vs sklearn perceptron
 
 import random
 import pylab
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Perceptron as SKPerceptron
+from sklearn.metrics import mean_absolute_error
 from perceptron import *
 
 class Point:
@@ -46,59 +49,84 @@ def classify_points(D, f):
             negativeD.append(pt)
     return positiveD, negativeD
 
+def get_sk_line(skperc):
+    assert isinstance(skperc, SKPerceptron),\
+        "Wrong perceptron type (must be sklearn.linear_model.perceptron)"
+    w = skperc.coef_[0]
+    slope = -w[0] / w[1]
+    x_intercept = -skperc.intercept_ / w[1]
+    return slope, x_intercept
+
 # Driver
 if __name__ == "__main__":
     # Generate points and plot them
     random.seed(0)
     D = generate_points(100)
-    pylab.title("Single Perceptron Convergence")
-    pylab.xlabel("x1")
-    pylab.ylabel("x2")
-    #pylab.plot([pt.x1 for pt in D], \
+    fig = pylab.figure(figsize=(7,7))
+    mainplt = fig.add_subplot(111)
+    mainplt.set_title("Single Perceptron Binary Classifier (built vs sklearn convergence)")
+    mainplt.set_xlabel("x1")
+    mainplt.set_ylabel("x2")
+    #mainplt.plot([pt.x1 for pt in D], \
     #           [pt.x2 for pt in D], \
     #           "bo", label = "D = Sample data points")
     
     # Fix target function (unkown in real-life application)
     f = lambda x: -0.3 * x + 35 # target function (hand picked)
-    pylab.plot([Point.lo_lim, Point.up_lim], \
+    mainplt.plot([Point.lo_lim, Point.up_lim], \
                [f(Point.lo_lim), f(Point.up_lim)], \
                "grey", label = "f(x) = target function")
-    pylab.legend()
+    mainplt.legend()
     
     # Calculate D points classification (positive or negative)
     # Positive points: Above target function
     # Negative points: Bellow target function
     posD, negD = classify_points(D, f)
-    pylab.plot([pt.x1 for pt in posD], [pt.x2 for pt in posD], "bo")
-    pylab.plot([pt.x1 for pt in negD], [pt.x2 for pt in negD], "rx")
-    X = np.array([(1, pt.x1, pt.x2) for pt in posD + negD])
+    mainplt.plot([pt.x1 for pt in posD], [pt.x2 for pt in posD], "bo")
+    mainplt.plot([pt.x1 for pt in negD], [pt.x2 for pt in negD], "rx")
+    X = np.array([(pt.x1, pt.x2) for pt in posD + negD])
     y = np.array([1] * len(posD) + [-1] * len(negD))
     
-    # Start PLA
+    # Start PLA (use built Perceptron vs. sklearn Perceptron)
     # (1) Split dataset into training and test datasets 80/20
     # (2) Fit perceptron using training dataset
     # (3) Test predictions using test dataset + plot g final hypothesis
     # Stage 1
-    perc = Perceptron(2)
+    perc = Perceptron(n_features=2)
+    skperc = SKPerceptron(max_iter=10000)
     X_train, X_test, y_train, y_test = \
         train_test_split(X, y, test_size=0.2, random_state=1)
     # Stage 2
     perc.fit(X_train, y_train)
-    print("Model fit:", perc)
-    print("Iterations made:", perc.iters)
+    skperc.fit(X_train, y_train)
+    print("Model fit (built):", perc, "| Model fit (sklearn):", skperc.coef_[0])
+    print("Iterations made (built):", perc.iters)
     # Stage 3
     # Predict on train data
-    correct, wrong = perc.test_predictions(X_train, y_train)
-    print("Correct predictions on train data:", correct)
-    print("Wrong predictions on train data:", wrong)
+    built_predicted_in_sample = perc.predict(X_train)
+    built_in_sample_score = mean_absolute_error(y_train, built_predicted_in_sample)
+    print("In Sample Score (built):", built_in_sample_score)
+    sk_predicted_in_sample = skperc.predict(X_train)
+    sk_in_sample_score = mean_absolute_error(y_train, sk_predicted_in_sample)
+    print("In Sample Score (sklearn):", sk_in_sample_score)
     # Predict test data
-    correct, wrong = perc.test_predictions(X_test, y_test)
-    print("Correct predictions on test data:", correct)
-    print("Wrong predictions on test data:", wrong)
-    # PLot g
-    slope, intersect = perc.get_line()
-    g = lambda x: slope * x + intersect
-    pylab.plot([Point.lo_lim, Point.up_lim], \
+    built_predicted_out_sample = perc.predict(X_test)
+    built_out_sample_score = mean_absolute_error(y_test, built_predicted_out_sample)
+    print("Out of Sample Score (built):", built_out_sample_score)
+    sk_predicted_out_sample = skperc.predict(X_test)
+    sk_out_sample_score = mean_absolute_error(y_test, sk_predicted_out_sample)
+    print("Out of Sample Score (sklearn):", sk_out_sample_score)
+
+    # PLot g and g'
+    slope, x_intersect = perc.get_line()
+    g = lambda x: slope * x + x_intersect
+    mainplt.plot([Point.lo_lim, Point.up_lim], \
                [g(Point.lo_lim), g(Point.up_lim)], \
-               "purple", label = "g(x) = final hypothesis")
-    pylab.legend()
+               "purple", label = "g(x) = final hypothesis by built perceptron")
+    sk_slope, sk_x_intercept = get_sk_line(skperc)
+    g2 = lambda x: sk_slope * x + sk_x_intercept
+    mainplt.plot([Point.lo_lim, Point.up_lim], \
+               [g2(Point.lo_lim), g2(Point.up_lim)], \
+               "green", label = "g'(x) = final hypothesis by built sklearn")
+    mainplt.legend()
+    fig.savefig('final_plot.png', dpi = 300)
--- PLA/First_PLA/perceptron.py
+++ PLA/First_PLA/perceptron.py
@@ -1,7 +1,11 @@
+# Simple perceptron binary classifier implementation
+
 import numpy as np
 
 class Perceptron:
     def __init__(self, n_features):
+        assert n_features == 2, \
+               "Perceptron class is not ready for n_features != 2 (check get_line)"
         self.w = np.array([0] * (n_features+1))
         self.iters = 0
         
@@ -9,15 +13,28 @@ class Perceptron:
         return ", ".join(["x" + str(i) + "=" + str(self.w[i]) \
                           for i in range(len(self.w))])
         
+    # Auxiliary method
     def sign(self, val):
         if val > 0:
             return 1
         else:
             return -1
+    
+    # Calculates the sum of the multiplications between weights and params
+    def decision_function(self, X):
+        scores = []
+        for x in X:
+            score = np.dot(self.w.T, np.insert(x, 0, 1))
+            scores.append(score)
+        return scores
         
-    def predict(self, x):
-        score = np.dot(self.w.T, x)
-        return self.sign(score)
+    # Predict the classification of set of inputs (classify -1 or 1)
+    def predict(self, X):
+        classif = []
+        scores = self.decision_function(X)
+        for score in scores:
+            classif.append(self.sign(score))
+        return classif
     
     def fit(self, X, y):
         changes = -1
@@ -25,22 +42,12 @@ class Perceptron:
             changes = 0
             self.iters += 1
             for x, _y in zip(X, y):
-                if self.predict(x) != _y:
+                if self.predict([x])[0] != _y:
                     changes += 1
-                    self.w += _y * x
+                    self.w += _y * np.insert(x, 0, 1)
     
     def get_line(self):
         # TODO: generalize (only works for 2 features)
         slope = -self.w[1] / self.w[2]
-        intersect = -self.w[0] / self.w[2]
-        return slope, intersect
-    
-    def test_predictions(self, X, y):
-        correct, wrong = 0, 0
-        for x, y in zip(X, y):
-            predicted = self.predict(x)
-            if predicted == y:
-                correct += 1
-            else:
-                wrong += 1
-        return correct, wrong
+        x_intersect = -self.w[0] / self.w[2]
+        return slope, x_intersect
