ca69a6e633a671052c4a320d4cb06ca645fd641d
fabioiuri <fabioiuri@live.com>
2018-09-05 11:11:42 +0100

    Comment out 2nd missing data approach

--- Data_Preprocessing/Simple_Classification/main.py
+++ Data_Preprocessing/Simple_Classification/main.py
@@ -18,8 +18,8 @@ X[ : , 1:3] = imputer.transform(X[ : , 1:3]) # compute and add to dataset
 #print("1st missing data approach:\n", X)
 
 #   2nd approach: drop rows that contain some NaN value
-X = dataset.dropna().iloc[ : , :-1].values # drop rows with NaNs
-y =  dataset.dropna().iloc[ : , -1].values
+#X = dataset.dropna().iloc[ : , :-1].values # drop rows with NaNs
+#y =  dataset.dropna().iloc[ : , -1].values
 #print("2nd missing data approach:\n", X)
 
 # 2. Enconding categorical data (encode fields to be used within modle calcs)
@@ -48,8 +48,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_
 #    2. Compute variance (signa**2) = sum((x's-mew)**2)/N
 #    3. Compute std (sigma) = sqrt(variance)
 #    4. Normalized x = (x - mew) / sigma
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.fit_transform(X_test)
 print("X_train:\n", X_train)
 print("X_test:\n", X_test)
