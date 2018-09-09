ce0a2c5b5ee2265264f16a1d749c8aa0026726e7
fabioiuri <fabioiuri@live.com>
2018-09-09 18:37:58 +0100

    Move perceptron class to parent folder

--- Single_Perceptrons/First_PLA/main.py
+++ Single_Perceptrons/First_PLA/main.py
@@ -8,6 +8,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron as SKPerceptron
 from sklearn.metrics import mean_absolute_error
+
+import sys
+sys.path.append('../')
 from perceptron import Perceptron
 
 class Point:
